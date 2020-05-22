7937d81c5da65d849dd1bfcf85bce76d70400382
feliciano <felicianojrsousa@gmail.com>
2020-05-22 08:14:37 -0300

    estruturacao inicial do projeto

---
+++ src/main/java/br/com/vivo/GothamApplication.java
@@ -0,0 +1,13 @@
+package br.com.vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GothamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GothamApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/vivo/controller/cdr/CdrController.java
@@ -0,0 +1,7 @@
+package br.com.vivo.controller.cdr;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CdrController {
+}
